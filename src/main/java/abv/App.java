0c2a5e94f0a7ad4a28966425e4068daedbfdc804
aayushya <aayushya1@gmail.com>
2020-05-05 12:37:34 +0530

    Subtraction method added

--- src/main/java/abv/App.java
+++ src/main/java/abv/App.java
@@ -11,11 +11,17 @@ public class App
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Integer a = Integer.parseInt(br.readLine());
         Integer b = Integer.parseInt(br.readLine());
-        System.out.print("The Output is : ");
+        System.out.print("The Output of Addition is : ");
         System.out.println(addition(a,b));
+
+        System.out.print("The Output of Subtraction is : ");
+        System.out.println(subtraction(a,b));
     }
 
     public static Integer addition(Integer a,Integer b){
         return a+b;
+    }
+    public static Integer subtraction(Integer a,Integer b){
+        return  a-b;
     }
 }
